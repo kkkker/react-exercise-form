ed23db53f4d5772b392e843445156caa02e73d33
kker <907765631@qq.com>
2020-09-23 15:56:01 +0800

    [Mamingjun]: implement onChange

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -6,46 +6,76 @@ class MyProfile extends Component {
   state = {
     name: "Your name",
     gender: "Male",
-    description: "Description about yourselt",
+    description: "Description about yourself",
     isReadTheTerms: true,
+  };
+
+  handleChange = (name, event) => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  handleCheckedChange = (name, event) => {
+    this.setState({
+      [name]: event.target.checked,
+    });
   };
 
   render() {
     return (
       <div className="my-form">
-        <form >
+        <form>
           <h2>My Profile</h2>
           <h6>Name</h6>
           <input
             type="text"
             className="form-control"
             value={this.state.name}
+            onChange={(e) => this.handleChange("name", e)}
             id="name"
           />
           <h6>Gender</h6>
-          <select class="form-control" className="form-control" id="gender">
-            <option>Male</option>
-            <option>Female</option>
+          <select
+            className="form-control"
+            onChange={(e) => this.handleChange("gender", e)}
+            id="gender"
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
           </select>
           <h6>Description</h6>
           <input
             type="textarea"
-            className="form-control"
+            className="form-control textarea"
             value={this.state.description}
+            onChange={(e) => this.handleChange("description", e)}
             id="description"
           />
-          <div class="form-group form-check">
+          <div className="form-group form-check">
             <input
               type="checkbox"
               className="form-check-input"
               checked={this.state.isReadTheTerms}
+              onChange={(e) => this.handleCheckedChange("isReadTheTerms", e)}
               id="isReadTheTerms"
             />
             <label className="form-check-label" htmlFor="isReadTheTerms">
               I have read the terms of conduct
             </label>
           </div>
-          <button className="btn btn-primary">Submit</button>
+          <button
+            type="submit"
+            disabled={
+              !this.state.name ||
+              !this.state.gender ||
+              !this.state.description ||
+              !this.state.isReadTheTerms
+            }
+            className="btn btn-primary"
+          >
+            Submit
+          </button>
         </form>
       </div>
     );
