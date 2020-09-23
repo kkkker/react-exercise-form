c1c3f3e21957cd25180adab09cca4a17acd1469a
kker <907765631@qq.com>
2020-09-23 15:31:44 +0800

    [Mamingjun]: add div

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,31 +1,53 @@
 import React, { Component } from "react";
 import "./myProfile.less";
+import "bootstrap/dist/css/bootstrap.css";
 
 class MyProfile extends Component {
   state = {
     name: "Your name",
     gender: "Male",
     description: "Description about yourselt",
-    isReadTheTerms: true
+    isReadTheTerms: true,
   };
 
   render() {
     return (
-      <form>
-        <h2>My Profile</h2>
-        <h6>Name</h6>
-        <input type="text" value={this.state.name} id="name" />
-        <h6>Gender</h6>
-        <select class="form-control" id="gender">
-          <option>Male</option>
-          <option>Female</option>
-        </select>
-        <h6>Description</h6>
-        <input type="textarea" value={this.state.description} id="description" />
-        <input type="checkbox" checked={this.state.isReadTheTerms} id="isReadTheTerms" />
-        <label htmlFor="isReadTheTerms">I have read the terms of conduct</label>
-        <button>Submit</button>
-      </form>
+      <div className="my-form">
+        <form >
+          <h2>My Profile</h2>
+          <h6>Name</h6>
+          <input
+            type="text"
+            className="form-control"
+            value={this.state.name}
+            id="name"
+          />
+          <h6>Gender</h6>
+          <select class="form-control" className="form-control" id="gender">
+            <option>Male</option>
+            <option>Female</option>
+          </select>
+          <h6>Description</h6>
+          <input
+            type="textarea"
+            className="form-control"
+            value={this.state.description}
+            id="description"
+          />
+          <div class="form-group form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              checked={this.state.isReadTheTerms}
+              id="isReadTheTerms"
+            />
+            <label className="form-check-label" htmlFor="isReadTheTerms">
+              I have read the terms of conduct
+            </label>
+          </div>
+          <button className="btn btn-primary">Submit</button>
+        </form>
+      </div>
     );
   }
 }
